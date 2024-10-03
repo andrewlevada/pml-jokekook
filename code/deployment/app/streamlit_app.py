ce4b86d1b2b80fd222078bd45d4e2cde33daf8f0
natalia agapova <agapnatalya004@gmail.com>
2024-10-03 21:19:42 +0300

    added comments

--- code/deployment/app/streamlit_app.py
+++ code/deployment/app/streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import requests
 
-# Input
+# input
 input_text = st.text_input("A joke about...")
 
-# Button
 if st.button("Generate a joke"):
     prompt = f"A joke about {input_text}: "
+    # send prompt to api    
     response = requests.post("http://api:8000/generate", json={"prompt": prompt})
+    # output
     st.write(f"{response.json()['generated_text']}")
