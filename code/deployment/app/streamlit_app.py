512b6572cc5ada36ec5fc46a5ca615b3bcdfd30f
natalia agapova <agapnatalya004@gmail.com>
2024-10-03 21:11:59 +0300

    changed localhost to api

--- code/deployment/app/streamlit_app.py
+++ code/deployment/app/streamlit_app.py
@@ -7,5 +7,5 @@ input_text = st.text_input("A joke about...")
 # Button
 if st.button("Generate a joke"):
     prompt = f"A joke about {input_text}: "
-    response = requests.post("http://localhost:8000/generate", json={"prompt": prompt})
+    response = requests.post("http://api:8000/generate", json={"prompt": prompt})
     st.write(f"{response.json()['generated_text']}")
